0e942c5107f8bf4ca97a6e91c64156fa608d592e
Carl Truong <Carl@tabrific.com>
2016-04-29 11:01:03 -0600

    refactor HomeCtrl.js

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,9 +1,11 @@
 (function() {
 	function HomeCtrl($interval, Tasks) {
 		var vm = this;
+		vm.completedTask = "";
 		vm.tasks = Tasks.getTasks();
 		vm.addTask = function() {
 			Tasks.addTask(vm.completedTask);
+			vm.completedTask = "";
 		} 
 		
 	}
--- dist/scripts/controllers/HomeCtrl.js
+++ dist/scripts/controllers/HomeCtrl.js
@@ -1,12 +1,39 @@
 (function() {
 	function HomeCtrl($interval, Tasks) {
 		var vm = this;
+		vm.completedTask = "";
 		vm.tasks = Tasks.getTasks();
 		vm.addTask = function() {
 			Tasks.addTask(vm.completedTask);
+			vm.completedTask = "";
 		} 
 		
 	}
+	
+//	function HomeCtrl($interval, Tasks) {
+//		var vm = this;
+//		
+//		vm.addTask = addTask;
+//		vm.completedTask = null;
+//		vm.tasks = Tasks.getTasks();
+//		
+//		console.count("Activating implicitly");
+//		activate();
+//		
+//		function activate() {
+//			console.count("Activating explicitly");
+//			resetNewTaskInput();
+//		}
+//		
+//		function resetNewTaskInput() {
+//			vm.completedTask = (new Date()).toISOString();
+//		}
+//		
+//		function addTask() {
+//			Tasks.addTask(vm.completedTask);
+//			resetNewTaskInput();
+//		}
+//	}
 		
 	angular
 		.module('pomodoro')
